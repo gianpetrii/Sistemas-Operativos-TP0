b9c30a96f06b034f92a0b3e1fe27dd8d1a43bd70
Gianluca Petri <gpetri@frba.utn.edu.ar>
2023-04-15 19:51:47 +0000

    Creo logger, escribo algo y lo destruyo al final de ejecucion

--- client/src/client.c
+++ client/src/client.c
@@ -18,7 +18,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Hola! Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -54,8 +54,14 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
+	// Inicializo mi nuevo logger
 	t_log* nuevo_logger;
 
+	// Chequeo que mi nuevo logger no sea nulo para asegurar no error
+	if ((nuevo_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO)) == NULL){
+		printf("No se creo bien el nuevo logger\n");
+		exit(1);	// lo hago para que se pausee la ejecucion con error
+	}
 	return nuevo_logger;
 }
 
@@ -97,4 +103,9 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+
+	// Pregunto si tengo un logger existente y si lo tengo lo destruyo
+	if (logger != NULL){
+		log_destroy(logger);
+	}
 }
